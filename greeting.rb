f019ca8171ff3873228cf60aeb15a24e202571a2
Alex Rodriguez <alexrod102797@gmail.com>
2019-08-14 21:14:18 +0000

    Automatically backed up by Learn

---
+++ greeting.rb
@@ -0,0 +1,18 @@
+def greeting_a_person(name)
+  puts "Hello #{name}"
+end
+
+greeting_a_person("Maria")
+
+english_music_by_city = {
+  :manchester => [
+    {
+      :band_name => "The Smiths",
+      :member_names => ["Morrissey", "Johnny", "Andy", "Mike"]
+    },
+    {
+      :band_name => "Joy Division",
+      :member_names => ["Peter", "Stephen", "Bernard", "Ian"]
+    },
+  ]
+}
